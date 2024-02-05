bbbcbc42ca7bb4b1dfc57ccd401b1395fb059e09
yavrum13 <yuval.avrum@gmail.com>
2024-02-04 23:55:41 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  const books = fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then(books => {
+      renderBooks(books);
+    });
+  return books;
   
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
